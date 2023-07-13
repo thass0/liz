99a61e6726a0c9d07a45eb6214745301dabe2392
Thassilo Schulze <thassilo.schulze@posteo.de>
2023-07-13 14:41:48 +0200

    Create thread for session

---
+++ src/bot.rs
@@ -0,0 +1,137 @@
+pub struct Bot {
+    _db: PgPool,
+}
+
+impl Bot {
+    pub async fn new(db: PgPool) -> Result<Self, CustomError> {
+        db.execute(include_str!("../schema.sql"))
+            .await
+            .map_err(CustomError::new)?;
+
+        Ok(Bot { _db: db })
+    }
+}
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn ready(&self, ctx: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+
+        let guild_id = GUILD_ID;
+        let commands = GuildId::set_application_commands(
+            &guild_id,
+            &ctx.http,
+            |commands| {
+                commands
+                    .create_application_command(|command| {
+                        command
+                            .name(CMD_EVAL)
+                            .description("Evaluate an S-expression")
+                            .create_option(|option| {
+                                option
+                                    .name(CMD_EVAL_SEXPR)
+                                    .description("S-expression to evaluate")
+                                    .kind(CommandOptionType::String)
+                                    .required(true)
+                            })
+                    })
+                    .create_application_command(|command| {
+                        command
+                            .name(CMD_SESSION)
+                            .description("Enter a Lisp session")
+                    })
+            },
+        )
+        .await
+        .unwrap();
+
+        info!("{:#?}", commands);
+    }
+
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            let response_content = match command.data.name.as_str() {
+                CMD_EVAL => {
+                    let arg = command
+                        .data
+                        .options
+                        .iter()
+                        .find(|opt| opt.name == CMD_EVAL_SEXPR)
+                        .cloned();
+                    let value = arg.unwrap().value.unwrap();
+                    let sexpr_str = value.as_str().unwrap();
+                    eval(sexpr_str)
+                },
+                CMD_SESSION => {
+                    let name =
+                        Generator::with_naming(Name::Plain).next().unwrap();
+                    let send_followup =
+                        command.channel_id.say(&ctx.http, "Yay, let's code");
+                    match send_followup.await {
+                        Err(why) => {
+                            error!(
+                                "Failed to send thread follow-up message: {}",
+                                why
+                            );
+                            "Can't create thread : (".to_owned()
+                        },
+                        Ok(follow_up) => {
+                            let create_thread =
+                                command.channel_id.create_public_thread(
+                                    &ctx.http,
+                                    follow_up.id,
+                                    |thread| thread.name(&name),
+                                );
+                            match create_thread.await {
+                                Err(why) => {
+                                    error!("Failed to create thread:  {}", why);
+                                    "Can't create thread : (".to_owned()
+                                },
+                                Ok(_channel) => {
+                                    "Creating a thread for you :D".to_owned()
+                                },
+                            }
+                        },
+                    }
+                },
+                command => unreachable!("Unknown command: {}", command),
+            };
+
+            let create_response =
+                command.create_interaction_response(&ctx.http, |response| {
+                    response
+                        .kind(InteractionResponseType::ChannelMessageWithSource)
+                        .interaction_response_data(|message| {
+                            message.content(response_content)
+                        })
+                });
+
+            if let Err(why) = create_response.await {
+                error!("Cannot respond to slash command: {}", why);
+            }
+        }
+    }
+}
+
+const CMD_EVAL: &str = "eval";
+const CMD_EVAL_SEXPR: &str = "sexpr";
+
+const CMD_SESSION: &str = "session";
+
+const GUILD_ID: GuildId = GuildId(1128676502272229468);
+
+use names::{Generator, Name};
+use serenity::async_trait;
+use serenity::model::application::interaction::{
+    Interaction,
+    InteractionResponseType,
+};
+use serenity::model::gateway::Ready;
+use serenity::model::prelude::command::CommandOptionType;
+use serenity::model::prelude::GuildId;
+use serenity::prelude::*;
+use shuttle_runtime::CustomError;
+use sqlx::{Executor, PgPool};
+use tracing::{error, info};
+
+use crate::eval::eval;
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,7 @@
-use anyhow::anyhow;
-use serenity::async_trait;
-use serenity::model::application::interaction::{
-    Interaction,
-    InteractionResponseType,
-};
-use serenity::model::channel::Message;
-use serenity::model::gateway::Ready;
-use serenity::model::prelude::command::CommandOptionType;
-use serenity::model::prelude::GuildId;
-use serenity::prelude::*;
-use shuttle_secrets::SecretStore;
-use tracing::{error, info};
-
-use crate::eval::eval;
-
-pub mod eval;
-
-struct Bot;
-
-const CMD_EVAL: &str = "eval";
-const CMD_EVAL_SEXPR: &str = "sexpr";
-
-#[async_trait]
-impl EventHandler for Bot {
-    // Every time our bot receives a message, it adds this
-    // message to the eval.
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
-    async fn ready(&self, ctx: Context, ready: Ready) {
-        info!("{} is connected!", ready.user.name);
-
-        let guild_id = GuildId(1128676502272229468);
-        let commands = GuildId::set_application_commands(
-            &guild_id,
-            &ctx.http,
-            |commands| {
-                commands.create_application_command(|command| {
-                    command
-                        .name(CMD_EVAL)
-                        .description("Evaluate an S-expression")
-                        .create_option(|option| {
-                            option
-                                .name(CMD_EVAL_SEXPR)
-                                .description("S-expression to evaluate")
-                                .kind(CommandOptionType::String)
-                                .required(true)
-                        })
-                })
-            },
-        )
-        .await
-        .unwrap();
-
-        info!("{:#?}", commands);
-    }
-
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            let response_content = match command.data.name.as_str() {
-                CMD_EVAL => {
-                    let arg = command
-                        .data
-                        .options
-                        .iter()
-                        .find(|opt| opt.name == CMD_EVAL_SEXPR)
-                        .cloned();
-                    let value = arg.unwrap().value.unwrap();
-                    let sexpr_str = value.as_str().unwrap();
-                    eval(sexpr_str)
-                },
-                command => unreachable!("Unknown command: {}", command),
-            };
-
-            let create_response =
-                command.create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| {
-                            message.content(response_content)
-                        })
-                });
-
-            if let Err(why) = create_response.await {
-                error!("Cannot respond to slash command: {}", why);
-            }
-        }
-    }
-}
-
 #[shuttle_runtime::main]
 async fn serenity(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    #[shuttle_shared_db::Postgres] pool: PgPool,
 ) -> shuttle_serenity::ShuttleSerenity {
     // Get the discord token set in `Secrets.toml`
     let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
@@ -110,10 +15,26 @@ async fn serenity(
     let intents =
         GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
+    let bot = match Bot::new(pool).await {
+        Ok(bot) => bot,
+        Err(e) => {
+            return Err(e.into());
+        },
+    };
     let client = Client::builder(&token, intents)
-        .event_handler(Bot)
+        .event_handler(bot)
         .await
         .expect("Err creating client");
 
     Ok(client.into())
 }
+
+use anyhow::anyhow;
+use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
+use sqlx::PgPool;
+
+mod bot;
+mod eval;
+
+use crate::bot::Bot;
